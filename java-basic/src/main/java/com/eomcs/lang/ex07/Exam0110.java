2521858508b6f2202089344347ea7aff50e0ece4
Gyeryeong Ban <gyeryeong.ban@outlook.com>
2021-07-14 19:21:46 +0900

    Day 13

---
+++ java-basic/src/main/java/com/eomcs/lang/ex07/Exam0110.java
@@ -0,0 +1,44 @@
+package com.eomcs.lang.ex07;
+
+import java.util.Scanner;
+
+//# 메서드 : 사용 전
+//
+public class Exam0110 {
+
+  static void printSpaces(int len) {
+    for (int i = 0; i < len; i++) {
+      System.out.print(" ");
+    }
+  }
+
+  static void printStars(int len) {
+    for (int i = 0; i < len; i++) {
+      System.out.print("*");
+
+    }
+  }
+
+  static int getSpaceLenght(int totalStar, int displayStar) {
+    return (totalStar - displayStar) / 2;
+  }
+
+  public static void main(String[] args) {
+    Scanner keyScan = new Scanner(System.in);
+    System.out.print("밑변의 길이? ");
+    int len = keyScan.nextInt();
+    keyScan.close();
+
+
+    for (int starLen = 1; starLen <= len; starLen += 2) {
+      // 별 앞에 공백 출력
+
+      // 별 출력
+      printStars(starLen);
+
+      // 출력 줄 바꾸기
+      System.out.println();
+
+    }
+  }
+}
