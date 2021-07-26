0f1fd606fcb5d73c3866a50036bf834be61fee8c
Gyeryeong Ban <gyeryeong.ban@outlook.com>
2021-07-26 14:57:45 +0900

    Day 21

--- study-project/app/src/main/java/com/eomcs/pms/App.java
+++ study-project/app/src/main/java/com/eomcs/pms/App.java
@@ -10,14 +10,10 @@ public class App {
 
   public static void main(String[] args) {
 
-    // 각 게시판의 게시글을 담을 메모리(boards 레퍼런스 배열과 size)를 준비한다.
-    // new BoardHandler()
-    // - BoardHandler가 작업할 때 사용할 변수를 준비한다.
-    // - BoardHandler는 게시글을 다루는 작업을 한다.
+    BoardHandler boardHandler = new BoardHandler();
     MemberHandler memberHandler = new MemberHandler();
     ProjectHandler projectHandler = new ProjectHandler();
     TaskHandler taskHandler = new TaskHandler();
-    BoardHandler boardHandler = new BoardHandler();
 
     while (true) {
       String input = Prompt.inputString("명령> ");
@@ -31,27 +27,59 @@ public class App {
       } else if (input.equals("/member/list")) {
         memberHandler.list();
 
-      } else if (input.equals("/project/add")) {
-        projectHandler.add(memberHandler);
+      } else if (input.equals("/member/detail")) {
+        memberHandler.detail();
 
-      } else if (input.equals("/project/list")) {
+      } else if (input.equals("/member/update")) {
+        memberHandler.update();
+
+      } else if (input.equals("/member/delete")) {
+        memberHandler.delete();
+
+      }  else if (input.equals("/project/add")) {
+        projectHandler.add();
+
+      }  else if (input.equals("/project/list")) {
         projectHandler.list();
 
-      } else if (input.equals("/task/add")) {
-        taskHandler.add(memberHandler);
+      }  else if (input.equals("/project/detail")) {
+        projectHandler.detail();
 
-      } else if (input.equals("/task/list")) {
+      }  else if (input.equals("/project/update")) {
+        projectHandler.update();
+
+      }  else if (input.equals("/project/delete")) {
+        projectHandler.delete();
+
+      }  else if (input.equals("/task/add")) {
+        taskHandler.add();
+
+      }  else if (input.equals("/task/list")) {
         taskHandler.list();
 
-      } else if (input.equals("/board/add")) {
-        // BoardHandler의 add()를 실행할 때
-        // add()에서 사용할 게시글 배열이 있는 인스턴스 주소를 넘겨준다.
+      }  else if (input.equals("/task/detail")) {
+        taskHandler.detail();
+
+      }  else if (input.equals("/task/update")) {
+        taskHandler.update();
+
+      }  else if (input.equals("/task/delete")) {
+        taskHandler.delete();
+
+      }  else if (input.equals("/board/add")) {
         boardHandler.add();
 
-      } else if (input.equals("/board/list")) {
-        // BoardHandler의 list()를 실행할 때
-        // list()에서 사용할 게시글 배열이 있는 인스턴스 주소를 넘겨준다.
+      }  else if (input.equals("/board/list")) {
         boardHandler.list();
+
+      }  else if (input.equals("/board/detail")) {
+        boardHandler.detail();
+
+      }  else if (input.equals("/board/update")) {
+        boardHandler.update();
+
+      }  else if (input.equals("/board/delete")) {
+        boardHandler.delete();
 
       } else {
         System.out.println("실행할 수 없는 명령입니다.");
@@ -59,7 +87,19 @@ public class App {
       System.out.println();
     }
 
-    // Prompt 가 소유하고 관리하고 있는 자원을 닫으라고 명령한다.
+    // Prompt 가 소유하고 관리하고 있는 자원을 닫으라고 명령한다. 
     Prompt.close();
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
