97006b2d2c28f68ae8e919186b89aed8db24ebf5
Gyeryeong Ban <gyeryeong.ban@outlook.com>
2021-08-10 18:42:16 +0900

    Day 32

--- java-basic/src/main/java/com/eomcs/basic/ex01/Exam0174.java
+++ java-basic/src/main/java/com/eomcs/basic/ex01/Exam0174.java
@@ -55,7 +55,7 @@ public class Exam0174 {
     Engine engine = new Engine(3000, 16);
     Car car = new Car("비트자동차", "비트비트", engine);
 
-    // 자동차 복제
+    // 자동차 복제 
     // => Car의 clone()에서 Engine 객체도 복제할 것이다.
     Car car2 = car.clone();
 
