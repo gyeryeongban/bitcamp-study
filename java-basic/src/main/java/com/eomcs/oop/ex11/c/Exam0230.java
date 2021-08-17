9f5a13c727b8f61ed1198757ad233df68faa82b8
Gyeryeong Ban <gyeryeong.ban@outlook.com>
2021-08-17 15:32:39 +0900

    Day 36

--- java-basic/src/main/java/com/eomcs/oop/ex11/c/Exam0230.java
+++ java-basic/src/main/java/com/eomcs/oop/ex11/c/Exam0230.java
@@ -1,4 +1,4 @@
-// inner class : 바깥 클래스의 인스턴스 멤버 접근하기 2
+// inner class : 바깥 클래스의 인스턴스 멤버 접근하기 II
 package com.eomcs.oop.ex11.c;
 
 class B3 {
@@ -12,9 +12,9 @@ class B3 {
     void test() {
       int v1 = 1000;
 
-      System.out.printf("v1 = %d\n", v1); // 로컬 변수
-      System.out.printf("this.v1 = %d\n", this.v1); // 인스턴스 변수
-      System.out.printf("B3.this.v1 = %d\n", B3.this.v1); // 바깥 객체의 인스턴스 변수
+      System.out.printf("v1 = %d\n", v1); // 로컬 변수 
+      System.out.printf("this.v1 = %d\n", this.v1); // 인스턴스 변수 
+      System.out.printf("B3.this.v1 = %d\n", B3.this.v1); // 바깥 객체의 인스턴스 변수 
     }
   }
 }
@@ -25,12 +25,12 @@ public class Exam0230 {
     B3 outer = new B3();
     outer.v1 = 11;
 
-    B3.X obj = outer.new X();
-    obj.test();
+    B3.X x1 = outer.new X();
+    x1.test();
     System.out.println("--------------------");
 
-    B3.X obj2 = outer.new X();
-    obj2.test();
+    B3.X x2 = outer.new X();
+    x2.test();
     System.out.println("--------------------");
 
     B3 outer2 = new B3();
@@ -39,7 +39,6 @@ public class Exam0230 {
     B3.X x3 = outer2.new X();
     x3.test();
     System.out.println("--------------------");
-
   }
 
 }
