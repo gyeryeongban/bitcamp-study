4fb130180165a1946dec39d73bdfceae2b1070dc
Gyeryeong Ban <gyeryeong.ban@outlook.com>
2021-10-13 12:12:39 +0900

    Update Day 72

---
+++ java-basic/src/main/java/com/eomcs/exception/A.java
@@ -0,0 +1,7 @@
+package com.eomcs.exception;
+
+public class A {
+  public void m() throws Exception {
+    throw new Exception();
+  }
+}
---
+++ java-basic/src/main/java/com/eomcs/exception/Test.java
@@ -0,0 +1,12 @@
+package com.eomcs.exception;
+
+public class Test {
+  public static void main(String[] args) {
+    A a = new A();
+    try {
+      a.m();
+    } catch (Exception e) {
+
+    }
+  }
+}
