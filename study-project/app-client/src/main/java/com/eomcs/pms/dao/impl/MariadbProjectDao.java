4682bb76055985fd6aea402737ca0e479619516a
Gyeryeong Ban <gyeryeong.ban@outlook.com>
2021-10-18 14:32:54 +0900

    Update Day 75

--- study-project/app-client/src/main/java/com/eomcs/pms/dao/impl/MariadbProjectDao.java
+++ study-project/app-client/src/main/java/com/eomcs/pms/dao/impl/MariadbProjectDao.java
@@ -26,92 +26,72 @@ public class MariadbProjectDao implements ProjectDao {
 
   }
 
-  // 방법1: 프로젝트 목록을 가져올 때
+  // 방법1: 프로젝트 목록을 가져올 때 멤버 목록도 함께 가져오기
   @Override
   public List<Project> findAll() throws Exception {
     try (PreparedStatement stmt = con.prepareStatement(
-        "select" 
+        "select"
             + " p.project_no,"
             + " p.title,"
+            + " p.content,"
             + " p.start_dt,"
             + " p.end_dt,"
-            + " m.member_no,"
-            + " m.name,"
-            + " m.email"
-            + " from pms_project p"
+            + " m.member_no owner_no,"
+            + " m.name owner_name,"
+            + " m.email owner_email,"
+            + " m2.member_no member_no,"
+            + " m2.name member_name,"
+            + " m2.email member_email"
+            + " from" 
+            + " pms_project p"
             + " inner join pms_member m on p.member_no=m.member_no"
-            + " order by project_no desc");
-        PreparedStatement stmt2 = con.prepareStatement(
-            "select\r\n"
-                + " m.project_no,"
-                + " m.member_no,"
-                + " m.name,"
-                + " m.email"
-                + " from"
-                + " pms_project_member pm"
-                + " inner join pms_member m on pm.member_no=m.member_no"
-                + " where"
-                + " pm.project_no=101"
-                + " order by"
-                + " m.name asc);");
+            + " left outer join pms_project_member pm on p.project_no=pm.project_no"
+            + " inner join pms_member m2 on pm.member_no=m2.member_no"
+            + " "
+            + " order by" 
+            + " project_no desc, m2.name asc");
         ResultSet rs = stmt.executeQuery()) {
-
-      /*
-       select
-          pm.project_no,
-          m.member_no,
-          m.name,
-          m.email
-          m2.member_no,
-          m2.name
-          m2.email
-       from
-          pms_project_member pm
-          inner join pms_member m on pm.member_no=m.member_no
-          left outer join pms_project_member pm on p.project_no=pm.project_no
-          inner join pms_member m2 on pm.member=m2.member_no
-        order by project_no desc;
-        where
-          pm.project_no=101
-        order by
-          m.name asc);
-       */
 
       ArrayList<Project> list = new ArrayList<>();
 
+      int projectNo = 0;
+      Project project = null;
+
       while (rs.next()) {
-        Project project = new Project();
-        project.setNo(rs.getInt("project_no"));
-        project.setTitle(rs.getString("title"));
-        project.setStartDate(rs.getDate("start_dt"));
-        project.setEndDate(rs.getDate("end_dt"));
+        if (projectNo != rs.getInt("project_no")) {
+          project = new Project();
+          project.setNo(rs.getInt("project_no"));
+          project.setTitle(rs.getString("title"));
+          project.setContent(rs.getString("content"));
+          project.setStartDate(rs.getDate("start_dt"));
+          project.setEndDate(rs.getDate("end_dt"));
 
-        Member owner = new Member();
-        owner.setNo(rs.getInt("member_no"));
-        owner.setName(rs.getString("name"));
-        owner.setEmail(rs.getString("email"));
+          Member owner = new Member();
+          owner.setNo(rs.getInt("owner_no"));
+          owner.setName(rs.getString("owner_name"));
+          owner.setEmail(rs.getString("owner_email"));
 
-        project.setOwner(owner);
+          project.setOwner(owner);
 
-        stmt2.setInt(1, project.getNo());
-        try (ResultSet memberRs = stmt2.executeQuery()) {
-          while (memberRs.next()) {
-            Member member = new Member();
-            member.setNo(memberRs.getInt("member_no"));
-            member.setName(memberRs.getString("name"));
-            member.setEmail(memberRs.getString("member_email"));
-          }
+          list.add(project);
+          projectNo = project.getNo();
         }
 
-
-        list.add(project);
+        // 프로젝트의 멤버가 있다면 기존 멤버 목록에 추가한다.
+        if (rs.getString("member_name") != null) {
+          Member member = new Member();
+          member.setNo(rs.getInt("member_no"));
+          member.setName(rs.getString("member_name"));
+          member.setEmail(rs.getString("member_email"));
+          project.getMembers().add(member);
+        }
       }
 
-      return list;
+      return project;
     }
   }
 
-  // 방법2:
+  // 방법2: 프로젝트 목록을 가져온 후 각 프로젝트에 대해 멤버 목록을 가져오기
   //  @Override
   //  public List<Project> findAll() throws Exception {
   //    try (PreparedStatement stmt = con.prepareStatement(
@@ -127,34 +107,19 @@ public class MariadbProjectDao implements ProjectDao {
   //            + " inner join pms_member m on p.member_no=m.member_no"
   //            + " order by project_no desc");
   //        PreparedStatement stmt2 = con.prepareStatement(
-  //            "select\r\n"
-  //                + " m.project_no,"
+  //            "select" 
+  //                + " pm.project_no, "
   //                + " m.member_no,"
   //                + " m.name,"
   //                + " m.email"
-  //                + " from"
+  //                + " from "
   //                + " pms_project_member pm"
   //                + " inner join pms_member m on pm.member_no=m.member_no"
   //                + " where"
-  //                + " pm.project_no=101"
-  //                + " order by"
-  //                + " m.name asc);");
+  //                + " pm.project_no=?"
+  //                + " order by "
+  //                + " m.name asc");
   //        ResultSet rs = stmt.executeQuery()) {
-  //
-  //      /*
-  //       select
-  //          pm.project_no,
-  //          m.member_no,
-  //          m.name,
-  //          m.email
-  //       from
-  //          pms_project_member pm
-  //          inner join pms_member m on pm.member_no=m.member_no
-  //        where
-  //          pm.project_no=101
-  //        order by
-  //          m.name asc);
-  //       */
   //
   //      ArrayList<Project> list = new ArrayList<>();
   //
@@ -172,16 +137,18 @@ public class MariadbProjectDao implements ProjectDao {
   //
   //        project.setOwner(owner);
   //
+  //        // 프로젝트의 멤버 목록 가져오기
   //        stmt2.setInt(1, project.getNo());
   //        try (ResultSet memberRs = stmt2.executeQuery()) {
   //          while (memberRs.next()) {
   //            Member member = new Member();
   //            member.setNo(memberRs.getInt("member_no"));
   //            member.setName(memberRs.getString("name"));
-  //            member.setEmail(memberRs.getString("member_email"));
+  //            member.setEmail(memberRs.getString("email"));
+  //
+  //            project.getMembers().add(member);
   //          }
   //        }
-  //
   //
   //        list.add(project);
   //      }
