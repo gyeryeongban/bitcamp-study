e6bad59c3d64a1f5c14d65737f3acb7fb2034b06
Gyeryeong Ban <gyeryeong.ban@outlook.com>
2021-10-18 14:10:15 +0900

    Update Day 75

--- study-project/app-client/src/main/java/com/eomcs/pms/dao/impl/MariadbProjectDao.java
+++ study-project/app-client/src/main/java/com/eomcs/pms/dao/impl/MariadbProjectDao.java
@@ -26,6 +26,7 @@ public class MariadbProjectDao implements ProjectDao {
 
   }
 
+  // 방법1: 프로젝트 목록을 가져올 때
   @Override
   public List<Project> findAll() throws Exception {
     try (PreparedStatement stmt = con.prepareStatement(
@@ -40,7 +41,41 @@ public class MariadbProjectDao implements ProjectDao {
             + " from pms_project p"
             + " inner join pms_member m on p.member_no=m.member_no"
             + " order by project_no desc");
+        PreparedStatement stmt2 = con.prepareStatement(
+            "select\r\n"
+                + " m.project_no,"
+                + " m.member_no,"
+                + " m.name,"
+                + " m.email"
+                + " from"
+                + " pms_project_member pm"
+                + " inner join pms_member m on pm.member_no=m.member_no"
+                + " where"
+                + " pm.project_no=101"
+                + " order by"
+                + " m.name asc);");
         ResultSet rs = stmt.executeQuery()) {
+
+      /*
+       select
+          pm.project_no,
+          m.member_no,
+          m.name,
+          m.email
+          m2.member_no,
+          m2.name
+          m2.email
+       from
+          pms_project_member pm
+          inner join pms_member m on pm.member_no=m.member_no
+          left outer join pms_project_member pm on p.project_no=pm.project_no
+          inner join pms_member m2 on pm.member=m2.member_no
+        order by project_no desc;
+        where
+          pm.project_no=101
+        order by
+          m.name asc);
+       */
 
       ArrayList<Project> list = new ArrayList<>();
 
@@ -58,12 +93,102 @@ public class MariadbProjectDao implements ProjectDao {
 
         project.setOwner(owner);
 
+        stmt2.setInt(1, project.getNo());
+        try (ResultSet memberRs = stmt2.executeQuery()) {
+          while (memberRs.next()) {
+            Member member = new Member();
+            member.setNo(memberRs.getInt("member_no"));
+            member.setName(memberRs.getString("name"));
+            member.setEmail(memberRs.getString("member_email"));
+          }
+        }
+
+
         list.add(project);
       }
 
       return list;
     }
   }
+
+  // 방법2:
+  //  @Override
+  //  public List<Project> findAll() throws Exception {
+  //    try (PreparedStatement stmt = con.prepareStatement(
+  //        "select" 
+  //            + " p.project_no,"
+  //            + " p.title,"
+  //            + " p.start_dt,"
+  //            + " p.end_dt,"
+  //            + " m.member_no,"
+  //            + " m.name,"
+  //            + " m.email"
+  //            + " from pms_project p"
+  //            + " inner join pms_member m on p.member_no=m.member_no"
+  //            + " order by project_no desc");
+  //        PreparedStatement stmt2 = con.prepareStatement(
+  //            "select\r\n"
+  //                + " m.project_no,"
+  //                + " m.member_no,"
+  //                + " m.name,"
+  //                + " m.email"
+  //                + " from"
+  //                + " pms_project_member pm"
+  //                + " inner join pms_member m on pm.member_no=m.member_no"
+  //                + " where"
+  //                + " pm.project_no=101"
+  //                + " order by"
+  //                + " m.name asc);");
+  //        ResultSet rs = stmt.executeQuery()) {
+  //
+  //      /*
+  //       select
+  //          pm.project_no,
+  //          m.member_no,
+  //          m.name,
+  //          m.email
+  //       from
+  //          pms_project_member pm
+  //          inner join pms_member m on pm.member_no=m.member_no
+  //        where
+  //          pm.project_no=101
+  //        order by
+  //          m.name asc);
+  //       */
+  //
+  //      ArrayList<Project> list = new ArrayList<>();
+  //
+  //      while (rs.next()) {
+  //        Project project = new Project();
+  //        project.setNo(rs.getInt("project_no"));
+  //        project.setTitle(rs.getString("title"));
+  //        project.setStartDate(rs.getDate("start_dt"));
+  //        project.setEndDate(rs.getDate("end_dt"));
+  //
+  //        Member owner = new Member();
+  //        owner.setNo(rs.getInt("member_no"));
+  //        owner.setName(rs.getString("name"));
+  //        owner.setEmail(rs.getString("email"));
+  //
+  //        project.setOwner(owner);
+  //
+  //        stmt2.setInt(1, project.getNo());
+  //        try (ResultSet memberRs = stmt2.executeQuery()) {
+  //          while (memberRs.next()) {
+  //            Member member = new Member();
+  //            member.setNo(memberRs.getInt("member_no"));
+  //            member.setName(memberRs.getString("name"));
+  //            member.setEmail(memberRs.getString("member_email"));
+  //          }
+  //        }
+  //
+  //
+  //        list.add(project);
+  //      }
+  //
+  //      return list;
+  //    }
+  //  }
 
   @Override
   public Project findByNo(int no) throws Exception {
