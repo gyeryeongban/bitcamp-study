6c3f82ccab95dc8b2d1c81bd68b4c6447e72c9c5
Gyeryeong Ban <gyeryeong.ban@outlook.com>
2021-11-09 11:54:44 +0900

    [1109] Update Day 89

--- eomcs-servlet/app/src/main/java/com/eomcs/web/ex01/Servlet03.java
+++ eomcs-servlet/app/src/main/java/com/eomcs/web/ex01/Servlet03.java
@@ -3,6 +3,7 @@ package com.eomcs.web.ex01;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,16 +16,17 @@ import javax.servlet.http.HttpServletResponse;
 // => HTTP 프로토콜을 다루려면 GenericServlet을 상속 받지 말고 
 //    HttpServlet을 상속 받아 서블릿 클래스를 만들라!
 // 
+@WebServlet("/ex01/s3")
 public class Servlet03 extends HttpServlet {
-  
+
   // GenericServlet 추상 클래스가 java.io.Serialize 인터페이스를 구현하였고,
   // HttpServlet 클래스가 GenericServlet 추상 클래스를 상속 받았으니
   // HttpServlet 클래스를 상속 받는 이 클래스도 마찬가지로
   // serialVersionUID 변수 값을 설정해야 한다.
   private static final long serialVersionUID = 1L;
 
-  // service()를 오버라이딩 하는 대신에
-  // doGet(), doPost(), doHead() 등을 오버라이딩 하라.
+  // Servlet 인터페이스의 service()를 오버라이딩 하는 대신에
+  // 
   // 호출과정:
   // => 웹브라우저
   //   => 톰캣 서버
@@ -32,7 +34,7 @@ public class Servlet03 extends HttpServlet {
   //       => Serlvet03.service(HttpServletRequest, HttpServletResponse)
   //         => Servlet03.doGet(HttpServletRequest, HttpServletResponse)
   @Override
-  public void doGet(HttpServletRequest req, HttpServletResponse res)
+  public void service(HttpServletRequest req, HttpServletResponse res)
       throws ServletException, IOException {
     System.out.println("Servlet03.doGet(HttpServletRequest,HttpServletResponse)");
   }
